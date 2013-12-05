c3a40e4710c96a1a15e7cf2c7df7df4a13b6e4bd
Lab41PaulM <paulm@lab41.org>
2013-12-05 14:54:29 -0800

    refactoring

--- setup.py
+++ setup.py
@@ -9,17 +9,16 @@ class MyInstall(install):
     def run(self):
         install.run(self)
         print "Installing dependencies"
-        os.system('sudo /usr/local/bin/install_script.sh')
 
 setup(
-    name='RedwoodProject',
+    name='RedwoodUtility',
     version='0.1.0',
     author='Lab41',
     author_email='paulm@lab41.org',
     description='A project that implements statistical methods for identifying anomalous files.',
     url='http://lab41.github.io/Redwood',
-    packages=['redwood', 'redwood.filters', 'redwood.io','redwood.helpers', 'redwood.connection', 'redwood.foundation'],
-    scripts=['bin/shell/run.py', 'scripts/install_script.sh'],
+    packages=['redwood', 'redwood.filters', 'redwood.shell','redwood.io','redwood.helpers', 'redwood.connection', 'redwood.foundation'],
+    scripts=['bin/redwood'],
     license='LICENSE.txt',
     long_description=open('README.md').read(),
     keywords='redwood stats statistics anomalies'.split(),
@@ -34,6 +33,7 @@ setup(
         ('', ['LICENSE.txt'])
     ],
     install_requires=[
-        'scipy'
+        'numpy',
+        'scipy',
     ]
 )
