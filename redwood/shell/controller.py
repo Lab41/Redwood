43cead6ca79cbf187014aae861402c268499e33f
Charlie Lewis <clewis@iqt.org>
2013-12-19 21:44:14 +0000

    basics in place, still needs a lot of cleanup and wiring to real functions

--- redwood/shell/controller.py
+++ redwood/shell/controller.py
@@ -1,44 +1,75 @@
+import cmd
 import sys
-import os
-import getopt
-import string
-import exceptions
-import multiprocessing
-from modes import StandardMode
-from redwood.filters import filter_list
 import redwood.helpers.core as core
+import redwood.io.csv_importer as csv_load
+from redwood.filters import filter_list
+from modes import SubInterpreterFilter
 
-class SessionController:
+class SessionController(cmd.Cmd):
 
-    def __init__(self, cnx):
+    def default(self, line):
+        if line == 'EOF' or line == 'exit':
+            self.do_quit(line)
+            return True
+
+    def emptyline(self):
+        pass
+
+    def preloop(self, cnx=None):
         self.cnx = cnx
-        self.curr_filter_index = None
-        self.mode_stack = [StandardMode(cnx, self)]
-        
-    def pushMode(self, new_mode):
-        self.mode_stack.append(new_mode)
-    def popMode(self):
-        if(len(self.mode_stack) > 1):
-            self.mode_stack.pop()        
-    def run(self):
-        
-        print "...running with {} cores".format(multiprocessing.cpu_count())
-        
-        print "...loading filters from ./Filters directory if exists"
-        core.import_filters("./Filters", self.cnx)
 
-        while True:
-            mode = self.mode_stack[len(self.mode_stack) - 1] 
-            command = raw_input(mode.prompt)
-            command = string.strip(command)
-            args = string.split(command)
-           
-            if(len(args) == 0):
-                mode.do_help()
-                continue
-           
+    def cmdloop(self):
+        doQuit = False
+        while doQuit != True:
+            try:
+                self.prompt = '\033[1;32mredwood$ \033[1;m'
+                core.import_filters("./Filters", self.cnx)
+                return cmd.Cmd.cmdloop(self)
+            except KeyboardInterrupt:
+                sys.stdout.write('\n')
 
-            mode.execute(*args)
-            
+    def do_filter(self, line):
+        '''[*] filter\n\t|--activates FILTER mode:'''
+        sub_cmd = SubInterpreterFilter()
+        sub_cmd.cmdloop()
 
+    def do_import_filters(self, line):
+        '''[*] import_filters <path>\n\t|-[path]   - path to the directory containing the filters'''
+        print self.cnx
+        new_filters = core.import_filters(path, self.cnx)
+        print "New Filters: "
+        print new_filters
 
+    def do_load_csv(self, line):
+        '''[*] load_csv <path> <include-survey>
+            |-[path]   - path where csv files exist or a path to a csv file
+            |-[survey] - either set as \"yes\" or \"no\" if you want to include the survey'''
+        try:
+            line = line.strip().split()
+            path = line[0]
+            survey = line[1]
+            choice = False
+
+            if survey in ( "y", "Y", "yes", "Yes", "YES", "1" ) :
+                choice = True
+            elif survey in ( "n", "N", "no", "No", "NO", "0" ) :
+                choice = False
+            else:
+                print "Error: Please specify \"yes\" or \"no\" if you want a survey"
+                return
+
+            csv_load.run(self.cnx, path, choice)
+        except:
+            print "Error: Please specify a path and whether or not to include the survey"
+        return
+
+    def do_greet(self, line):
+        for plugin in filter_list:
+            print plugin.name
+
+    def do_quit(self, line):
+        '''quit: Exit the redwood console'''
+        if self.cnx != None:
+            self.cnx.close()
+        sys.stdout.write('\n')
+        return True
