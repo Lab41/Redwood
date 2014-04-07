6dc76d246bd113002f41cc0e592300f6f87eaeaf
cglewis <charliel@lab41.org>
2014-04-07 15:49:21 -0700

    fix indents

--- redwood/foundation/report.py
+++ redwood/foundation/report.py
@@ -50,16 +50,16 @@ class Report():
         for f in filter_list:
             f.cnx = self.cnx
         print f.name
-            path = f.run_survey(self.source.source_name)
-            try:
-                shutil.rmtree(self.report_dir + "/" + self.source.source_name + "/filters/" + f.name)
-            except:
-                pass
+        path = f.run_survey(self.source.source_name)
+        try:
+            shutil.rmtree(self.report_dir + "/" + self.source.source_name + "/filters/" + f.name)
+        except:
+            pass
 
         if path == None:
-        continue
+            continue
 
-            shutil.move(path, self.report_dir + "/" + self.source.source_name + "/filters/" + f.name)
+        shutil.move(path, self.report_dir + "/" + self.source.source_name + "/filters/" + f.name)
 
     def generate_report(self):
         report_dir = "reports/" + self.source.source_name
